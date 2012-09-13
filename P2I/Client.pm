274ebe2cbdc775153200d4f3a22024cbcd0c833c
Matthias Bethke <matthias@towiski.de>
2012-09-12 20:08:18 -0500

    Fixed field types

--- P2I/Client.pm
+++ P2I/Client.pm
@@ -17,38 +17,35 @@
 use Moose;
 use MooseX::Declare;
 
-class client {
-    use DateTime::Format::MySQL;
+class P2I::Client {
     use MooseX::Types::DateTime::ButMaintained qw/ DateTime /;
 
     has id           => (is => 'ro', isa => 'Int');
-    has parent_id    => (is => 'ro', isa => 'Int');
+    has parent_id    => (is => 'ro', isa => 'Maybe[Int]');
     has type         => (is => 'ro', isa => 'Str');
-    has cr_date      => (is => 'ro', isa => 'DateTime', writer => '_set_cr_date');
-    has cname        => (is => 'ro', isa => 'Str');
-    has pname        => (is => 'ro', isa => 'Str');
-    has login        => (is => 'ro', isa => 'Str');
-    has account_id   => (is => 'ro', isa => 'Int');
+    has cr_date      => (is => 'ro', isa => 'P2I::MysqlDateTime', coerce => 1);
+    has cname        => (is => 'ro', isa => 'Maybe[Str]');
+    has pname        => (is => 'ro', isa => 'Maybe[Str]');
+    has login        => (is => 'ro', isa => 'Maybe[Str]');
+    has account_id   => (is => 'ro', isa => 'Maybe[Int]');
     has status       => (is => 'ro', isa => 'Int');
-    has phone        => (is => 'ro', isa => 'Str');
-    has fax          => (is => 'ro', isa => 'Str');
-    has email        => (is => 'ro', isa => 'Str');
-    has address      => (is => 'ro', isa => 'Str');
-    has city         => (is => 'ro', isa => 'Str');
-    has state        => (is => 'ro', isa => 'Str');
-    has pcode        => (is => 'ro', isa => 'Str');
-    has country      => (is => 'ro', isa => 'Str');
+    has phone        => (is => 'ro', isa => 'Maybe[Str]');
+    has fax          => (is => 'ro', isa => 'Maybe[Str]');
+    has email        => (is => 'ro', isa => 'Maybe[Str]');
+    has address      => (is => 'ro', isa => 'Maybe[Str]');
+    has city         => (is => 'ro', isa => 'Maybe[Str]');
+    has state        => (is => 'ro', isa => 'Maybe[Str]');
+    has pcode        => (is => 'ro', isa => 'Maybe[Str]');
+    has country      => (is => 'ro', isa => 'Maybe[Str]');
     has locale       => (is => 'ro', isa => 'Str');
-    has limits_id    => (is => 'ro', isa => 'Int');
-    has params_id    => (is => 'ro', isa => 'Int');
-    has perm_id      => (is => 'ro', isa => 'Int');
-    has pool_id      => (is => 'ro', isa => 'Int');
-    has logo_id      => (is => 'ro', isa => 'Int');
-    has tmpl_id      => (is => 'ro', isa => 'Int');
-    has sapp_pool_id => (is => 'ro', isa => 'Int');
+    has limits_id    => (is => 'ro', isa => 'Maybe[Int]');
+    has params_id    => (is => 'ro', isa => 'Maybe[Int]');
+    has perm_id      => (is => 'ro', isa => 'Maybe[Int]');
+    has pool_id      => (is => 'ro', isa => 'Maybe[Int]');
+    has logo_id      => (is => 'ro', isa => 'Maybe[Int]');
+    has tmpl_id      => (is => 'ro', isa => 'Maybe[Int]');
+    has sapp_pool_id => (is => 'ro', isa => 'Maybe[Int]');
     has guid         => (is => 'ro', isa => 'Str');
-    has overuse      => (is => 'ro', isa => 'Bool');
-
-    sub set_cr_date { DateTime::Format::MySQL->parse_datetime(shift) }
+    has overuse      => (is => 'ro', isa => 'P2I::MysqlBool', coerce => 1);
 }
 
