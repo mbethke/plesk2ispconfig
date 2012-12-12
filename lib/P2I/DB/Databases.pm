949aa2902ec64cd5fd7602c5b444879912fc9ec2
Matthias Bethke <matthias@towiski.de>
2012-12-11 23:27:49 -0600

    Fixed JOIN; extra join to get the client's login

--- lib/P2I/DB/Databases.pm
+++ lib/P2I/DB/Databases.pm
@@ -8,11 +8,12 @@ class P2I::DB::Databases extends P2I::PleskDB {
         my ($doms, $sql) = $self->domain_sql('WHERE o.name');
         return map { P2I::Data::Database->new($_) } $self->query_hashes(q[
             SELECT d.name, d.type, d.db_server_id server,
-            u.login, a.type password_type, a.password
+            u.login, a.type password_type, a.password, c.login client_login
             FROM data_bases d
-            JOIN db_users u ON d.default_user_id=u.id
+            JOIN db_users u ON d.id=u.db_id
             JOIN accounts a ON u.account_id=a.id
             JOIN domains o on o.id=d.dom_id
+            JOIN clients c ON c.id=o.cl_id
             ] . $sql,
             @$doms
         );
