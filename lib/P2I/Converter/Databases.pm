de997d586bcfb134a2ae74d7cd4c8b11b06e14a4
Matthias Bethke <matthias@towiski.de>
2012-12-11 23:26:46 -0600

    Added actual conversion code

--- lib/P2I/Converter/Databases.pm
+++ lib/P2I/Converter/Databases.pm
@@ -2,16 +2,27 @@ use Modern::Perl;
 use MooseX::Declare;
 use Method::Signatures::Modifiers;
 
-class P2I::Converter::Databases extends P2I::Converter with P2I::Role::Database {
+class P2I::Converter::Databases extends P2I::Converter with P2I::Role::DatabaseCreator {
     use MooseX::Types::Moose ':all';
     use P2I::ISPconfigSOAP;
-
-    has domains     => (is => 'ro', isa => HashRef, default => sub { {} });
+    use Try::Tiny;
 
     method convert {
         $self->dbg(__PACKAGE__ . '::convert');
         
         for my $db ($self->db->get_databases) {
+            try {
+                $self->add_database(
+                    $self->get_client_id($db->client_login),
+                    {
+                        database    => $db->name,
+                        user        => $db->login,
+                        password    => $db->password,
+                    }
+                );
+            } catch {
+                print STDERR "$_\n";
+            };
         }
     }
 }
