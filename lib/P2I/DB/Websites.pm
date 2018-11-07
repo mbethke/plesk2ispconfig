74094a3fa1e1aad88a690b5458fd6687a15b6919
Richard Siddall <richard.siddall@elirion.net>
2018-11-07 14:51:57 -0500

    Schema change to handle removal of the subdomain table in Plesk now that subdomains are more like full web sites.

--- lib/P2I/DB/Websites.pm
+++ lib/P2I/DB/Websites.pm
@@ -6,7 +6,7 @@ class P2I::DB::Websites extends P2I::PleskDB {
 
     method list_website_ids {
         my ($doms, $sql) = $self->domain_sql(
-            "JOIN domains d ON d.id=h.dom_id WHERE d.name"
+            "JOIN domains d ON d.id=h.dom_id WHERE parentDomainId=0 AND d.name"
         );
         return $self->query_flat(qq[ SELECT DISTINCT dom_id FROM hosting h $sql], @$doms);
     }
@@ -18,6 +18,7 @@ class P2I::DB::Websites extends P2I::PleskDB {
                 h.python, h.fastcgi, h.miva, h.coldfusion, h.asp, h.asp_dot_net,
                 h.ssl, h.webstat, h.same_ssl, h.traffic_bandwidth,
                 h.max_connection, h.php_handler_type, h.www_root,
+                if(h.php='true', if (h.php_handler_type='fastcgi', 'fast-cgi', h.php_handler_type), 'none') as php_type,
                 d.name domain, d.htype,
                 s.login, s.home, s.shell, s.quota,
                 a.type password_type, a.password,
@@ -38,32 +39,34 @@ class P2I::DB::Websites extends P2I::PleskDB {
     }
 
     method get_subdomains_for_id(Int $dom_id) {
-        return $self->query_flat(q[ SELECT id FROM subdomains WHERE dom_id=? ],
+        return $self->query_flat(q[ SELECT id FROM domains WHERE parentDomainId=? ],
             $dom_id);
     }
 
     method get_subdomain(Int $id) {
         return $self->query_hash(q[
-           SELECT u.ssi, u.php, u.cgi, u.perl, u.python,
-                u.fastcgi, u.miva, u.coldfusion, u.asp, u.asp_dot_net,
-                u.ssl, u.same_ssl, u.name subdomain,
-                h.real_traffic, h.fp, h.fp_ssl, h.fp_enable, h.fp_adm,
-                h.webstat, h.traffic_bandwidth, h.fp_pass,
+            SELECT h.real_traffic, h.fp, h.fp_ssl, h.fp_enable, h.fp_adm,
+                h.fp_pass, h.ssi, h.php, h.cgi, h.perl,
+                h.python, h.fastcgi, h.miva, h.coldfusion, h.asp, h.asp_dot_net,
+                h.ssl, h.webstat, h.same_ssl, h.traffic_bandwidth,
                 h.max_connection, h.php_handler_type, h.www_root,
-                u.name sub_domain, CONCAT(u.name, '.', d.name) domain,
-                d.name parent_domain, d.htype,
+                if(h.php='true', if (h.php_handler_type='fastcgi', 'fast-cgi', h.php_handler_type), 'none') as php_type,
+                d.name domain, d.htype,
                 s.login, s.home, s.shell, s.quota,
                 a.type password_type, a.password,
                 i.ip_address,
                 t.csr ssl_csr, t.pvt_key ssl_key, t.cert ssl_cert, t.ca_cert ssl_cacert,
-                c.login client_login
-            FROM subdomains u
+                TRIM(TRAILING '.' FROM SUBSTRING_INDEX(d.name, p.name, 1)) subdomain,
+                c.login client_login,
+                p.name parent_domain
+            FROM hosting h
             JOIN domains d ON d.id=u.dom_id
             JOIN hosting h ON h.dom_id=u.dom_id
             JOIN sys_users s ON s.id=h.sys_user_id
             JOIN accounts a ON a.id=s.account_id
             JOIN IP_Addresses i ON i.id=IF(h.ip_address_id = 0, (select val from misc where param='def_ip_id'), h.ip_address_id)
             JOIN clients c ON c.id=d.cl_id
+            JOIN domains p ON p.id=d.parentDomainId
             LEFT OUTER JOIN certificates t ON t.id=h.certificate_id
             WHERE u.id=? ],
             $id);
